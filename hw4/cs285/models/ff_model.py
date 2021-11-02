c9f2d7d866c4e552298e1393d3a688436cff0482
Doinksta <erichliang08@gmail.com>
2021-11-02 11:23:30 -0700

    closer to 0.2 but not

--- hw4/cs285/models/ff_model.py
+++ hw4/cs285/models/ff_model.py
@@ -152,7 +152,7 @@ class FFModel(nn.Module, BaseModel):
             ptu.from_numpy(data_statistics['acs_std']),
             ptu.from_numpy(data_statistics['delta_mean']),
             ptu.from_numpy(data_statistics['delta_std'])
-        )[0])  # TODO(Q1) compute the loss
+        )[1])  # TODO(Q1) compute the loss
         # Hint: `self(...)` returns a tuple, but you only need to use one of the
         # outputs.
 
