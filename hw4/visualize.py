5612b48a60abccd4dda5785afa3b16b99e9ee2bb
Doinksta <erichliang08@gmail.com>
2021-11-04 03:32:35 -0700

    new plots

--- hw4/visualize.py
+++ hw4/visualize.py
@@ -69,9 +69,8 @@ logdir = '{}/hw4_q3_cheetah_cheetah-cs285-v0_03-11-2021_11-58-38/events*'.format
 eventfile = glob.glob(logdir)[0]
 
 X, Y = get_section_results(eventfile)
-plt.plot(range(len(X)), X, label='Train_AverageReturn', marker='o')
 plt.plot(range(len(Y)), Y, label='Eval_AverageReturn', marker='o')
-plt.title('Train and Eval Average Return for Cheetah')
+plt.title('Eval Average Return for Cheetah')
 plt.xlabel('Iteration #')
 plt.ylabel('Average Return')
 plt.legend()
@@ -83,22 +82,19 @@ plt.close()
 logdir = '{}/hw4_q4_reacher_ensemble1_reacher-cs285-v0_03-11-2021_11-04-27/events*'.format(root_folder)
 eventfile = glob.glob(logdir)[0]
 X, Y = get_section_results(eventfile)
-plt.plot(range(len(X)), X, label='Ensemble Size 1 Train_AverageReturn', marker='o')
 plt.plot(range(len(Y)), Y, label='Ensemble Size 1 Eval_AverageReturn', marker='o')
 
 logdir = '{}/hw4_q4_reacher_ensemble3_reacher-cs285-v0_03-11-2021_11-04-32/events*'.format(root_folder)
 eventfile = glob.glob(logdir)[0]
 X, Y = get_section_results(eventfile)
-plt.plot(range(len(X)), X, label='Ensemble Size 3 Train_AverageReturn', marker='o')
 plt.plot(range(len(Y)), Y, label='Ensemble Size 3 Eval_AverageReturn', marker='o')
 
 logdir = '{}/hw4_q4_reacher_ensemble5_reacher-cs285-v0_03-11-2021_11-04-21/events*'.format(root_folder)
 eventfile = glob.glob(logdir)[0]
 X, Y = get_section_results(eventfile)
-plt.plot(range(len(X)), X, label='Ensemble Size 5 Train_AverageReturn', marker='o')
 plt.plot(range(len(Y)), Y, label='Ensemble Size 5 Eval_AverageReturn', marker='o')
 
-plt.title('Ensemble Size vs. Train and Eval Average Return for Reacher')
+plt.title('Ensemble Size vs. Eval Average Return for Reacher')
 plt.xlabel('Iteration #')
 plt.ylabel('Average Return')
 plt.legend()
@@ -110,16 +106,14 @@ plt.close()
 logdir = '{}/hw4_q4_reacher_numseq100_reacher-cs285-v0_03-11-2021_02-16-23/events*'.format(root_folder)
 eventfile = glob.glob(logdir)[0]
 X, Y = get_section_results(eventfile)
-plt.plot(range(len(X)), X, label='Candidate Sequences 100 Train_AverageReturn', marker='o')
 plt.plot(range(len(Y)), Y, label='Candidate Sequences 100 Eval_AverageReturn', marker='o')
 
 logdir = '{}/hw4_q4_reacher_numseq1000_reacher-cs285-v0_03-11-2021_11-04-36/events*'.format(root_folder)
 eventfile = glob.glob(logdir)[0]
 X, Y = get_section_results(eventfile)
-plt.plot(range(len(X)), X, label='Candidate Sequences 1000 Train_AverageReturn', marker='o')
 plt.plot(range(len(Y)), Y, label='Candidate Sequences 1000 Eval_AverageReturn', marker='o')
 
-plt.title('# Candidate Action Sequences vs. Train and Eval Average Return for Reacher')
+plt.title('# Candidate Action Sequences vs. Eval Average Return for Reacher')
 plt.xlabel('Iteration #')
 plt.ylabel('Average Return')
 plt.legend()
@@ -131,22 +125,19 @@ plt.close()
 logdir = '{}/hw4_q4_reacher_horizon5_reacher-cs285-v0_03-11-2021_02-15-24/events*'.format(root_folder)
 eventfile = glob.glob(logdir)[0]
 X, Y = get_section_results(eventfile)
-plt.plot(range(len(X)), X, label='Horizon 5 Train_AverageReturn', marker='o')
 plt.plot(range(len(Y)), Y, label='Horizon 5 Eval_AverageReturn', marker='o')
 
 logdir = '{}/hw4_q4_reacher_horizon15_reacher-cs285-v0_03-11-2021_02-15-43/events*'.format(root_folder)
 eventfile = glob.glob(logdir)[0]
 X, Y = get_section_results(eventfile)
-plt.plot(range(len(X)), X, label='Horizon 15 Train_AverageReturn', marker='o')
 plt.plot(range(len(Y)), Y, label='Horizon 15 Eval_AverageReturn', marker='o')
 
 logdir = '{}/hw4_q4_reacher_horizon30_reacher-cs285-v0_03-11-2021_11-23-58/events*'.format(root_folder)
 eventfile = glob.glob(logdir)[0]
 X, Y = get_section_results(eventfile)
-plt.plot(range(len(X)), X, label='Horizon 30 Train_AverageReturn', marker='o')
 plt.plot(range(len(Y)), Y, label='Horizon 30 Eval_AverageReturn', marker='o')
 
-plt.title('Horizon Length vs. Train and Eval Average Return for Reacher')
+plt.title('Horizon Length vs. Eval Average Return for Reacher')
 plt.xlabel('Iteration #')
 plt.ylabel('Average Return')
 plt.legend()
@@ -158,22 +149,19 @@ plt.close()
 logdir = '{}/hw4_q5_cheetah_random_cheetah-cs285-v0_03-11-2021_20-08-19/events*'.format(root_folder)
 eventfile = glob.glob(logdir)[0]
 X, Y = get_section_results(eventfile)
-plt.plot(range(len(X)), X, label='Random Shooting Train_AverageReturn', marker='o')
 plt.plot(range(len(Y)), Y, label='Random Shooting Eval_AverageReturn', marker='o')
 
 logdir = '{}/hw4_q5_cheetah_cem_2_cheetah-cs285-v0_04-11-2021_00-09-42/events*'.format(root_folder)
 eventfile = glob.glob(logdir)[0]
 X, Y = get_section_results(eventfile)
-plt.plot(range(len(X)), X, label='CEM 2 Train_AverageReturn', marker='o')
 plt.plot(range(len(Y)), Y, label='CEM 2 Eval_AverageReturn', marker='o')
 
 logdir = '{}/hw4_q5_cheetah_cem_4_cheetah-cs285-v0_04-11-2021_00-09-55/events*'.format(root_folder)
 eventfile = glob.glob(logdir)[0]
 X, Y = get_section_results(eventfile)
-plt.plot(range(len(X)), X, label='CEM 4 Train_AverageReturn', marker='o')
 plt.plot(range(len(Y)), Y, label='CEM 4 Eval_AverageReturn', marker='o')
 
-plt.title('Action Selection vs. Train and Eval Average Return for Cheetah')
+plt.title('Action Selection vs. Eval Average Return for Cheetah')
 plt.xlabel('Iteration #')
 plt.ylabel('Average Return')
 plt.legend()
