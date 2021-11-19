b241d28897d0c994f507ece4905dd951a02f6b50
Doinksta <erichliang08@gmail.com>
2021-11-18 17:40:33 -0800

    regen fig

--- hw5/visualize.py
+++ hw5/visualize.py
@@ -3,31 +3,34 @@ from tensorflow.python.summary.summary_iterator import summary_iterator
 import matplotlib.pyplot as plt
 
 
-def get_section_results(file):
+def get_section_results(file, extra=None):
     """
         requires tensorflow==1.12.0
     """
     X = []
     Y = []
+    Z = []
     for e in summary_iterator(file):
         for v in e.summary.value:
             if v.tag == 'Train_EnvstepsSoFar':
                 X.append(v.simple_value)
             elif v.tag == 'Eval_AverageReturn':
                 Y.append(v.simple_value)
-    return X, Y
+            elif extra is not None and v.tag == extra:
+                Z.append(v.simple_value)
+    return (X, Y, Z) if extra is not None else (X, Y)
 
 
 root_folder = 'data_final'
 
 # Question 1 Subpart 1 Easy
 logdirs = [
-'{}/hw5_expl_q1_env1_random_PointmassEasy-v0_17-11-2021_17-03-27/events*'.format(root_folder),
-'{}/hw5_expl_q1_env1_rnd_PointmassEasy-v0_17-11-2021_17-03-19/events*'.format(root_folder)
+'{}/hw5_expl_q1_env1_random_PointmassEasy-v0_18-11-2021_17-00-15/events*'.format(root_folder),
+'{}/hw5_expl_q1_env1_rnd_PointmassEasy-v0_18-11-2021_17-00-07/events*'.format(root_folder)
 ]
 
 labels = [
-    'Epsilon Greedy Exploration',
+    'Random Exploration',
     'RND Exploration'
 ]
 
@@ -48,12 +51,12 @@ plt.close()
 
 # Question 1 Subpart 1 Medium
 logdirs = [
-'{}/hw5_expl_q1_env2_random_PointmassMedium-v0_17-11-2021_17-16-10/events*'.format(root_folder),
-'{}/hw5_expl_q1_env2_rnd_PointmassMedium-v0_17-11-2021_17-15-55/events*'.format(root_folder)
+'{}/hw5_expl_q1_env2_random_PointmassMedium-v0_18-11-2021_17-00-37/events*'.format(root_folder),
+'{}/hw5_expl_q1_env2_rnd_PointmassMedium-v0_18-11-2021_17-00-26/events*'.format(root_folder)
 ]
 
 labels = [
-    'Epsilon Greedy Exploration',
+    'Random Exploration',
     'RND Exploration'
 ]
 
@@ -74,8 +77,8 @@ plt.close()
 
 # Question 1 Subpart 2 Medium
 logdirs = [
-'{}/hw5_expl_q1_alg_med_PointmassMedium-v0_18-11-2021_03-07-34/events*'.format(root_folder),
-'{}/hw5_expl_q1_alg_med_rnd_PointmassMedium-v0_18-11-2021_03-07-46/events*'.format(root_folder)
+'{}/hw5_expl_q1_alg_med_PointmassMedium-v0_18-11-2021_17-01-11/events*'.format(root_folder),
+'{}/hw5_expl_q1_alg_med_rnd_PointmassMedium-v0_18-11-2021_17-01-21/events*'.format(root_folder)
 ]
 
 labels = [
@@ -100,8 +103,8 @@ plt.close()
 
 # Question 1 Subpart 2 Hard
 logdirs = [
-'{}/hw5_expl_q1_alg_hard_PointmassHard-v0_18-11-2021_02-45-59/events*'.format(root_folder),
-'{}/hw5_expl_q1_alg_hard_rnd_PointmassHard-v0_18-11-2021_02-37-29/events*'.format(root_folder)
+'{}/hw5_expl_q1_alg_hard_PointmassHard-v0_18-11-2021_17-01-31/events*'.format(root_folder),
+'{}/hw5_expl_q1_alg_hard_rnd_PointmassHard-v0_18-11-2021_17-01-41/events*'.format(root_folder)
 ]
 
 labels = [
