2fe6c52c84658d1cc9a14e819ea6ecb111cf62c3
Doinksta <erichliang08@gmail.com>
2021-11-18 18:52:11 -0800

    q2 part2

--- hw5/visualize.py
+++ hw5/visualize.py
@@ -1,6 +1,8 @@
 import glob
 from tensorflow.python.summary.summary_iterator import summary_iterator
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 
 
 def get_section_results(file, extra=None):
@@ -125,3 +127,154 @@ plt.legend()
 
 plt.savefig('plots/q1_hard2.png')
 plt.close()
+
+
+# Question 2 Subpart 1
+logdirs = [
+'{}/hw5_expl_q2_cql_PointmassMedium-v0_18-11-2021_16-18-12/events*'.format(root_folder),
+'{}/hw5_expl_q2_dqn_PointmassMedium-v0_18-11-2021_16-21-29/events*'.format(root_folder)
+]
+
+labels = [
+    'CQL',
+    'DQN'
+]
+
+for logdir, label in zip(logdirs, labels):
+    eventfile = glob.glob(logdir)[0]
+
+    X, Y, Z = get_section_results(eventfile, 'Exploitation_Data_q-values')
+    plt.plot(X, Y, label=label)
+
+plt.title('Exploration Method vs. Eval Average Return for PointmassMedium')
+plt.xlabel('Iteration #')
+plt.ylabel('Average Return')
+plt.legend()
+
+plt.savefig('plots/q2_learn1.png')
+plt.close()
+
+for logdir, label in zip(logdirs, labels):
+    eventfile = glob.glob(logdir)[0]
+
+    X, Y, Z = get_section_results(eventfile, 'Exploitation_Data_q-values')
+    plt.plot(X[3:], Z, label=label)
+
+plt.title('Exploration Method vs. In-Dataset Q-Values for PointmassMedium')
+plt.xlabel('Iteration #')
+plt.ylabel('Q Value')
+plt.legend()
+
+plt.savefig('plots/q2_qval1.png')
+plt.close()
+
+
+# Question 2 Subpart 2 CQL
+logdirs = [
+'{}/hw5_expl_q2_cql_numsteps_5000_PointmassMedium-v0_18-11-2021_17-48-10/events*'.format(root_folder),
+'{}/hw5_expl_q2_cql_numsteps_8000_PointmassMedium-v0_18-11-2021_17-48-18/events*'.format(root_folder),
+'{}/hw5_expl_q2_cql_numsteps_11000_PointmassMedium-v0_18-11-2021_17-48-25/events*'.format(root_folder),
+'{}/hw5_expl_q2_cql_numsteps_14000_PointmassMedium-v0_18-11-2021_17-48-31/events*'.format(root_folder)
+]
+
+labels = [
+    'CQL 5000 Steps',
+    'CQL 8000 Steps',
+    'CQL 11000 Steps',
+    'CQL 14000 Steps',
+]
+
+for logdir, label in zip(logdirs, labels):
+    eventfile = glob.glob(logdir)[0]
+
+    X, Y = get_section_results(eventfile)
+    plt.plot(X, Y, label=label)
+
+plt.title('CQL Number of Exploration Steps vs. Eval Average Return for PointmassMedium')
+plt.xlabel('Iteration #')
+plt.ylabel('Average Return')
+plt.legend()
+
+plt.savefig('plots/q2_explore_cql.png')
+plt.close()
+
+
+# Question 2 Subpart 2 DQN
+logdirs = [
+'{}/hw5_expl_q2_dqn_numsteps_5000_PointmassMedium-v0_18-11-2021_17-48-41/events*'.format(root_folder),
+'{}/hw5_expl_q2_dqn_numsteps_8000_PointmassMedium-v0_18-11-2021_17-48-48/events*'.format(root_folder),
+'{}/hw5_expl_q2_dqn_numsteps_11000_PointmassMedium-v0_18-11-2021_17-48-56/events*'.format(root_folder),
+'{}/hw5_expl_q2_dqn_numsteps_14000_PointmassMedium-v0_18-11-2021_17-49-05/events*'.format(root_folder)
+]
+
+labels = [
+    'DQN 5000 Steps',
+    'DQN 8000 Steps',
+    'DQN 11000 Steps',
+    'DQN 14000 Steps',
+]
+
+for logdir, label in zip(logdirs, labels):
+    eventfile = glob.glob(logdir)[0]
+
+    X, Y = get_section_results(eventfile)
+    plt.plot(X, Y, label=label)
+
+plt.title('DQN Number of Exploration Steps vs. Eval Average Return for PointmassMedium')
+plt.xlabel('Iteration #')
+plt.ylabel('Average Return')
+plt.legend()
+
+plt.savefig('plots/q2_explore_dqn.png')
+plt.close()
+
+
+# Question 2 Subpart 2 Table
+logdirs_cql = [
+'{}/hw5_expl_q2_cql_numsteps_5000_PointmassMedium-v0_18-11-2021_17-48-10/events*'.format(root_folder),
+'{}/hw5_expl_q2_cql_numsteps_8000_PointmassMedium-v0_18-11-2021_17-48-18/events*'.format(root_folder),
+'{}/hw5_expl_q2_cql_numsteps_11000_PointmassMedium-v0_18-11-2021_17-48-25/events*'.format(root_folder),
+'{}/hw5_expl_q2_cql_numsteps_14000_PointmassMedium-v0_18-11-2021_17-48-31/events*'.format(root_folder),
+]
+logdirs_dqn = [
+'{}/hw5_expl_q2_dqn_numsteps_5000_PointmassMedium-v0_18-11-2021_17-48-41/events*'.format(root_folder),
+'{}/hw5_expl_q2_dqn_numsteps_8000_PointmassMedium-v0_18-11-2021_17-48-48/events*'.format(root_folder),
+'{}/hw5_expl_q2_dqn_numsteps_11000_PointmassMedium-v0_18-11-2021_17-48-56/events*'.format(root_folder),
+'{}/hw5_expl_q2_dqn_numsteps_14000_PointmassMedium-v0_18-11-2021_17-49-05/events*'.format(root_folder)
+]
+
+cql_vals = []
+dqn_vals = []
+
+for logdir, label in zip(logdirs_cql, labels):
+    eventfile = glob.glob(logdir)[0]
+
+    X, Y = get_section_results(eventfile)
+    cql_vals.append(max(Y))
+
+for logdir, label in zip(logdirs_dqn, labels):
+    eventfile = glob.glob(logdir)[0]
+
+    X, Y = get_section_results(eventfile)
+    dqn_vals.append(max(Y))
+
+#define figure and axes
+fig, ax = plt.subplots()
+
+#hide the axes
+plt.gca().axis('off')
+plt.gca().axis('tight')
+
+plt.table(cellText=[cql_vals, dqn_vals],
+          rowLabels=['CQL', 'DQN'],
+          colLabels=['5000 Steps', '8000 Steps', '11000 Steps', '14000 Steps'],
+          loc='center'
+        )
+
+plt.title('Number of Exploration Steps vs. Max Eval Average Return')
+
+plt.tight_layout()
+
+plt.savefig('plots/q2_explore_table.png')
+plt.close()
+
